45610bbcb2d6f4d0487357667d2002f72087e47b
Allan Cruvinel <allan.cruvinel@gmail.com>
2020-03-25 03:21:03 -0300

    back end da aplicação

---
+++ backend/src/index.js
@@ -0,0 +1,23 @@
+const cors = require("cors");
+const express = require("express");
+const app = express();
+const routes = require('./routes');
+
+
+app.use(cors());
+app.use(express.json()); //faz a aplicação reconhecer objetos Json
+app.use(routes);
+/**
+ * Query Params : Parâmetros nomeados enviados na rota apos "?" (filtros,paginação)
+ * Route Params : Parâmetros utilizados para identificar recursos
+ * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
+ * exemplo Body
+ * 
+ * app.post('/users',function(request,response){
+    const body = request.body;
+    console.log(params);
+    return response.json(body);
+    });
+ */
+app.listen(3333);
+
